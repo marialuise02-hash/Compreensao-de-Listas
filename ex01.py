1e0591b9e3f386d0583cbc471d515798cfb42745
Maria Luíse das Virgens Menezes <marialuise02@gmail.com>
2026-06-22 13:34:28 -0300

    Fix list comprehension in lista_numerica function

--- ex01.py
+++ ex01.py
@@ -1,3 +1,5 @@
+# 1. Defina uma função que leia do teclado uma sequência de números inteiros dados em uma única linha. A função deve retornar uma lista contendo os números que estão na linha.
+
 def lista_numerica():
   return [map(int, input().split())]
 
