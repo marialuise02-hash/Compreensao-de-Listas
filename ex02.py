1294173a44648937b6c516e80d1d27ed0826a547
Maria Luíse das Virgens Menezes <marialuise02@gmail.com>
2026-06-22 13:40:13 -0300

    Implement 'descendentes' function for family tree
    Define the 'descendentes' function to return descendants from a family tree using list comprehensions.

--- ex02.py
+++ ex02.py
@@ -1,1 +1,5 @@
+# 2. Defina a função "descendentes" que pega uma árvore genealógica e retorna todos os descendentes da raiz. Utilize compreensões.
 
+def descendentes(integrantes):
+  integrantes = [input().split()]
+  return integrantes[2:]
