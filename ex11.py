b36f7f9ba50764badfd117df6e1e88ec1c9b55bb
Maria Luíse das Virgens Menezes <marialuise02@gmail.com>
2026-06-22 15:49:25 -0300

    Add functionality to count students above average

--- ex11.py
+++ ex11.py
@@ -1,1 +1,9 @@
+# 11. Dada uma lista com as notas de todos os alunos de uma turma, retorne a quantidade de alunos acima da média, que é 5
 
+notas = [int(x) for x in input().split()]
+
+acima_media = [nota for nota in notas if nota > 5]
+
+aprovados = len(acima_media)
+
+print(aprovados)
