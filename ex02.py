2381b5b1ef531af73a646be18189897520e295a6
Maria Luíse das Virgens Menezes <marialuise02@gmail.com>
2026-06-22 15:37:18 -0300

    Delete ex02.py

--- ex02.py
+++
@@ -1,7 +0,0 @@
-# 2. Defina a função "descendentes" que pega uma árvore genealógica e retorna todos os descendentes da raiz. Utilize compreensões.
-
-def descendentes(integrantes):
-  integrantes = [input().split()]
-  return integrantes[2:]
-
-print(descendentes(integrantes))
