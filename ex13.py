5885677ecb84e230c27b3c6cf8a1b1acefe55a32
Maria Luíse das Virgens Menezes <marialuise02@gmail.com>
2026-06-22 15:55:01 -0300

    Filter even numbers from integer matrix
    Add list comprehension to filter even numbers from matriz.

--- ex13.py
+++ ex13.py
@@ -1,1 +1,7 @@
 # 13. Dada uma matriz de inteiros, me faça uma lista com apenas os numeros pares da matriz
+
+matriz = []
+
+nums_pares = [num for num in matriz if num % 2 == 0]
+
+print(nums_pares)
