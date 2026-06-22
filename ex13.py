b8540403df1f561bcef7f162f2c5611bb95412a4
Maria Luíse das Virgens Menezes <marialuise02@gmail.com>
2026-06-22 16:01:27 -0300

    Refactor matrix input and filter even numbers
    Update the logic to read matrix input and extract even numbers.

--- ex13.py
+++ ex13.py
@@ -1,7 +1,9 @@
 # 13. Dada uma matriz de inteiros, me faça uma lista com apenas os numeros pares da matriz
 
-matriz = []
+tamanho_matriz = int(input())
 
-nums_pares = [num for num in matriz if num % 2 == 0]
+matriz = [[int(x) for i in input().split()] for i in range(tamanho_matriz)]
+
+nums_pares = [num for linha in matriz for num in linha if num % 2 == 0]
 
 print(nums_pares)
