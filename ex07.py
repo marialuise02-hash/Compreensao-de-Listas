696b2dfc29ddcc20ce70c649d302e665f290ca2c
Maria Luíse das Virgens Menezes <marialuise02@gmail.com>
2026-06-22 14:56:49 -0300

    Generate list of word lengths from input
    Fix list comprehension to calculate word lengths.

--- ex07.py
+++ ex07.py
@@ -1,1 +1,7 @@
+# 7. Gere uma lista contendo o tamanho de cada palavra. Ex de entrada: ["python", "java", "javascript", "c"]
 
+entrada = [input().split()]
+
+qtd_letras = [palavra for len(palavra) in entrada]
+
+print(qtd_letras)
